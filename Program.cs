97b4833f69520de9650c02ace6d3fdd264a1c3b1
Daniel Kenzo <103758337+kenzochan@users.noreply.github.com>
2025-01-08 13:13:56 -0300

    fix1

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FruitDb>(opt => opt.UseInMemoryDatabase("FruitList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Fruit API",
-        Description = "API for managing a list of fruit and their stock status.",
-    });
+    options.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Fruit API",
+            Description = "API for managing a list of fruit and their stock status.",
+        }
+    );
 });
 var app = builder.Build();
 
@@ -24,61 +26,70 @@ using (var scope = app.Services.CreateScope())
     dbContext.Database.EnsureCreated();
 }
 
- app.MapGet("/fruitlist",  async (FruitDb db) =>
-    await db.Fruits.ToListAsync())
-    .WithTags("Get all fruit"); 
+app.MapGet("/fruitlist", async (FruitDb db) => await db.Fruits.ToListAsync())
+    .WithTags("Get all fruit");
 
-app.MapGet("/fruitlist/instock", async (FruitDb db) =>
-    await db.Fruits.Where(t => t.Instock).ToListAsync())
+app.MapGet(
+        "/fruitlist/instock",
+        async (FruitDb db) => await db.Fruits.Where(t => t.Instock).ToListAsync()
+    )
     .WithTags("Get all fruit that is in stock");
 
-app.MapGet("/fruitlist/{id}", async (int id, FruitDb db) =>
-    await db.Fruits.FindAsync(id)
-        is Fruit fruit
-            ? Results.Ok(fruit)
-            : Results.NotFound())
+app.MapGet(
+        "/fruitlist/{id}",
+        async (int id, FruitDb db) =>
+            await db.Fruits.FindAsync(id) is Fruit fruit ? Results.Ok(fruit) : Results.NotFound()
+    )
     .WithTags("Get fruit by Id");
 
-app.MapPost("/fruitlist", async (Fruit fruit, FruitDb db) =>
-{
-    db.Fruits.Add(fruit);
-    await db.SaveChangesAsync();
+app.MapPost(
+        "/fruitlist",
+        async (Fruit fruit, FruitDb db) =>
+        {
+            db.Fruits.Add(fruit);
+            await db.SaveChangesAsync();
 
-    return Results.Created($"/fruitlist/{fruit.Id}", fruit);
-})
+            return Results.Created($"/fruitlist/{fruit.Id}", fruit);
+        }
+    )
     .WithTags("Add fruit to list");
 
-app.MapPut("/fruitlist/{id}", async (int id, Fruit inputFruit, FruitDb db) =>
-{
-    var fruit = await db.Fruits.FindAsync(id);
+app.MapPut(
+        "/fruitlist/{id}",
+        async (int id, Fruit inputFruit, FruitDb db) =>
+        {
+            var fruit = await db.Fruits.FindAsync(id);
 
-    if (fruit is null) return Results.NotFound();
+            if (fruit is null)
+                return Results.NotFound();
 
-    fruit.Name = inputFruit.Name;
-    fruit.Instock = inputFruit.Instock;
+            fruit.Name = inputFruit.Name;
+            fruit.Instock = inputFruit.Instock;
 
-    await db.SaveChangesAsync();
+            await db.SaveChangesAsync();
 
-    return Results.NoContent();
-})
+            return Results.NoContent();
+        }
+    )
     .WithTags("Update fruit by Id");
 
-app.MapDelete("/fruitlist/{id}", async (int id, FruitDb db) =>
-{
-    if (await db.Fruits.FindAsync(id) is Fruit fruit)
-    {
-        db.Fruits.Remove(fruit);
-        await db.SaveChangesAsync();
-        return Results.Ok(fruit);
-    }
+app.MapDelete(
+        "/fruitlist/{id}",
+        async (int id, FruitDb db) =>
+        {
+            if (await db.Fruits.FindAsync(id) is Fruit fruit)
+            {
+                db.Fruits.Remove(fruit);
+                await db.SaveChangesAsync();
+                return Results.Ok(fruit);
+            }
 
-    return Results.NotFound();
-})
+            return Results.NotFound();
+        }
+    )
     .WithTags("Delete fruit by Id");
-
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 
 app.Run();
